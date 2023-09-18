e6754b6d5ccb028d8a3e2e4afb4d7a8ada380c05
vito0182 <vitor.mnw@gmail.com>
2023-09-18 08:40:17 -0300

    now the main module can read txt files

--- main.py
+++ main.py
@@ -14,4 +14,21 @@ if __name__ == "__main__":
             n_days = compute_days.compute_days(date)
             print(f"\n Há {n_days} dias entre as duas datas")
 
-            continuar = input("\n - Deseja continuar? s/n: ").lower()
+            continuar = input("\n● Deseja continuar? s/n: ").lower()
+
+
+    else:
+        file_path = input("\n● Em qual arquivo estão as datas?: ")
+        
+        try:
+
+            with open(file_path, 'r') as file:
+                dates = file.readlines()
+
+                for date in dates:
+                    n_days = compute_days.compute_days(date)
+                    print(f"\n- Data: {date.strip()}" )
+                    print(f"- Há {n_days} dias entre as duas datas")
+
+        except FileNotFoundError:
+            print(f"O arquivo '{file}' não foi encontrado.")
