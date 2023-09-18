7047cf4fcc405db57f65b008633110b7c9b5f25b
vito0182 <vitor.mnw@gmail.com>
2023-09-18 09:05:04 -0300

    add program interface  module

--- main.py
+++ main.py
@@ -1,34 +1,4 @@
-import doctest
-import compute_days
+import program
 
 if __name__ == "__main__":
-    print("● Este programa calcula o número de dias entre duas datas")
-    print("● Insira as datas no formato do exemplo: '28 de Agosto de 2023 - 18 de Setembro de 2023'")
-
-    response = input("\n- Você deseja inserir a data pelo console? s/n: ").lower()
-    
-    if response in ["s", "sim"]:
-        continuar = "s" 
-        while continuar in ["s", "sim"]:
-            date = input("\nInforme as datas: ")
-            n_days = compute_days.compute_days(date)
-            print(f"\n Há {n_days} dias entre as duas datas")
-
-            continuar = input("\n● Deseja continuar? s/n: ").lower()
-
-
-    else:
-        file_path = input("\n● Em qual arquivo estão as datas?: ")
-        
-        try:
-
-            with open(file_path, 'r') as file:
-                dates = file.readlines()
-
-                for date in dates:
-                    n_days = compute_days.compute_days(date)
-                    print(f"\n- Data: {date.strip()}" )
-                    print(f"- Há {n_days} dias entre as duas datas")
-
-        except FileNotFoundError:
-            print(f"O arquivo '{file}' não foi encontrado.")
+    program.console_program()
---
+++ program.py
@@ -0,0 +1,48 @@
+import compute_days
+
+
+def compute_and_print_result(date):
+    n_days = compute_days.compute_days(date)
+    print(f"\n- Data: {date.strip()}" )
+    print(f"- Há {n_days} dias entre as duas datas")
+
+
+def dates_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            dates = file.readlines()
+            return dates
+        
+    except FileNotFoundError:
+        print(f"O arquivo '{file_path}' não foi encontrado.")
+
+
+def get_user_response():
+    response = input("\n- Você deseja inserir a data pelo console? s/n: ").lower()
+    run = False
+    if response in ["s", "sim", "y", "yes"]:
+        run = True
+
+    return run
+
+
+def console_program():
+
+    print("● Este programa calcula o número de dias entre duas datas")
+    print("● Insira as datas no formato do exemplo: '28 de Agosto de 2023 - 18 de Setembro de 2023'")
+
+    run = get_user_response()
+    
+    while run:
+        date = input("\nInforme as datas: ")
+        compute_and_print_result(date)
+        run = get_user_response()
+
+    else:
+
+        file_path = input("\n● Em qual arquivo estão as datas?: ")
+        
+
+        dates = dates_from_file(file_path)
+        for date in dates:
+            compute_and_print_result(date)
